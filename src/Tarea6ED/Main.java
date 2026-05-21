a519212088f26597d1c724042ec51fd2617b39fa
Ainhoa <ainhoa.garcia.96@gmail.com>
2026-05-21 14:13:16 +0200

    Commit desde CMD

---
+++ src/Tarea6ED/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Tarea6ED;
+
+/**
+ *
+ * @author Ainhoa Garcia
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    Tarea6EDClase miCarrito = new Tarea6EDClase();
+
+    miCarrito.aplicarDescuento(100, 5);
+}
+    }
+    
